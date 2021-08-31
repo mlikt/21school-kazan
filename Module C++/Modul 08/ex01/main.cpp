509da2c45b77a9ad8bd6ddd70203eeb801950980
Mathos Likt <mlikt@ev-j4.kzn.21-school.ru>
2021-08-31 20:53:59 +0300

    fix

--- Module C++/Modul 08/ex01/main.cpp
+++ Module C++/Modul 08/ex01/main.cpp
@@ -17,7 +17,7 @@ int main (void){
 	sp.addNumber(3);
 	sp.addNumber(17);
 	sp.addNumber(9);
-	sp.addNumber(11);
+	sp.addNumber(17);
 	std::cout << sp.shortestSpan() << std::endl;
 	std::cout << sp.longestSpan() << std::endl;
 
--- Module C++/Modul 08/ex01/span.cpp
+++ Module C++/Modul 08/ex01/span.cpp
@@ -13,7 +13,36 @@ void Span::addNumber(int const &n) {
 int Span::shortestSpan ( void ) const {
 	if (span.size() < 2)
 		throw SmallRange();
-	return(std::abs(*(++span.begin()) -  *(span.begin())));
+
+	std::multiset<int>::iterator it = span.begin();
+	std::multiset<int>::iterator it1 = ++span.begin();
+
+	int buf;
+	int out = *(it1) - *(it);
+
+	while (true)
+	{
+		if (out == 0)
+			return (out);
+		buf = *it1 - *it;
+		if (std::abs(buf) < std::abs(out))
+			out = buf;
+		std::advance(it, 2);
+		if (it == span.end())
+			break;
+		std::advance(it1, 2);
+		if (it1 == span.end())
+			break ;
+	}
+
+	if (N % 2)
+	{
+		buf = *span.rbegin() - *(++span.rbegin());
+		if (std::abs(buf) < std::abs(out))
+			out = buf;
+	}
+
+	return(std::abs(out));
 }
 
 int Span::longestSpan ( void ) const {
