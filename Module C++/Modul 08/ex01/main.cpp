18b677e06b71fd027bed46e0f59b0ba462417355
Mathos Likt <mlikt@ev-j4.kzn.21-school.ru>
2021-08-31 23:38:11 +0300

    fix

--- Module C++/Modul 08/ex01/main.cpp
+++ Module C++/Modul 08/ex01/main.cpp
@@ -16,8 +16,8 @@ int main (void){
 	sp.addNumber(5);
 	sp.addNumber(3);
 	sp.addNumber(17);
-	sp.addNumber(9);
-	sp.addNumber(17);
+	sp.addNumber(6);
+	sp.addNumber(12);
 	std::cout << sp.shortestSpan() << std::endl;
 	std::cout << sp.longestSpan() << std::endl;
 
--- Module C++/Modul 08/ex01/span.cpp
+++ Module C++/Modul 08/ex01/span.cpp
@@ -20,26 +20,15 @@ int Span::shortestSpan ( void ) const {
 	int buf;
 	int out = *(it1) - *(it);
 
-	while (true)
+	while (it1 != span.end())
 	{
 		if (out == 0)
 			return (out);
 		buf = *it1 - *it;
 		if (std::abs(buf) < std::abs(out))
 			out = buf;
-		std::advance(it, 2);
-		if (it == span.end())
-			break;
-		std::advance(it1, 2);
-		if (it1 == span.end())
-			break ;
-	}
-
-	if (N % 2)
-	{
-		buf = *span.rbegin() - *(++span.rbegin());
-		if (std::abs(buf) < std::abs(out))
-			out = buf;
+		it++;
+		it1++;
 	}
 
 	return(std::abs(out));
